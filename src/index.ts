3551bebdd0199c35cc09c091f97d1e4666bf1bdc
Junchi <benson26365156@gmail.com>
2023-08-19 14:49:03 +0800

    feat: add express initial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+import express from 'express'
+import type { Request, Response } from 'express'
+const app = express()
+
+app.get('/', (_req: Request, res: Response): void => {
+  res.send('Hello World')
+})
+
+app.listen(3000, (): void => {
+  console.log('server is run 3000 port')
+})
